37c24c5fc833c27142c0359f7b11159dbb9f69d6
DEEPAK J A <44841937+deepaksanjay@users.noreply.github.com>
2019-03-18 14:32:55 +0530

    Create AddTwoNumber program
    Create AddTwoNumber program

---
+++ AddTwoNumber.java
@@ -0,0 +1,10 @@
+public class AddTwoNumber{
+  public static void main(String args[]){
+    int c=0;
+    c=add(4,5);
+    System.out.println(c);
+  }
+  public static int add(int x,int y){
+     return x+y;
+  }
+}
